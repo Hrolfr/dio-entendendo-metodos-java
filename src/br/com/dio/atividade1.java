1a4a87bb3e15645f8a33aa4b6b6ac437e0010205
hrolfr <sabrina.sales163@gmail.com>
2022-07-16 13:22:02 -0300

    pasta e parte da atividade1

---
+++ src/br/com/dio/atividade1.java
@@ -0,0 +1,34 @@
+package br.com.dio;
+
+public class atividade1 {
+
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 6;
+
+        int soma = a + b;
+        System.out.println(soma);
+
+        int subtracao = a - b;
+        System.out.println(subtracao);
+
+        int multiplicacao = a * b;
+        System.out.println(multiplicacao);
+
+        float divisao = a / b;
+        System.out.println(divisao);
+
+    }
+
+     private static void hora(){
+
+        String hora1 = "8horas";
+         System.out.println("Bom dia!" +  hora1);
+
+        String hora2 = "12horas";
+         System.out.println("Boa tarde!" + hora2);
+
+     }
+
+}
