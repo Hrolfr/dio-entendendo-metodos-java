a8fd01b6b9678a6c22911fc5f7984eb2b752bdac
hrolfr <sabrina.sales163@gmail.com>
2022-07-16 14:40:15 -0300

    conta e hora

--- src/br/com/dio/atividade1.java
+++ src/br/com/dio/atividade1.java
@@ -4,31 +4,44 @@ public class atividade1 {
 
     public static void main(String[] args) {
 
-        int a = 12;
-        int b = 6;
-
-        int soma = a + b;
-        System.out.println(soma);
-
-        int subtracao = a - b;
-        System.out.println(subtracao);
-
-        int multiplicacao = a * b;
-        System.out.println(multiplicacao);
-
-        float divisao = a / b;
-        System.out.println(divisao);
+        System.out.println("Contas");
+        conta();
+        System.out.println("Hora");
+        hora();
+       /* System.out.println("Atribuição");
+        atribuicao();
+        System.out.println("Precedência");
+        precedencia();*/
 
     }
+        private static  void conta() {
+            int a = 12;
+            int b = 6;
 
-     private static void hora(){
+            int soma = a + b;
+            System.out.println(soma);
+
+            int subtracao = a - b;
+            System.out.println(subtracao);
+
+            int multiplicacao = a * b;
+            System.out.println(multiplicacao);
+
+            float divisao = a / b;
+            System.out.println(divisao);
+        }
+
+
+     private static void hora() {
 
         String hora1 = "8horas";
-         System.out.println("Bom dia!" +  hora1);
+         System.out.println("Bom dia! " +  hora1);
 
         String hora2 = "12horas";
-         System.out.println("Boa tarde!" + hora2);
+         System.out.println("Boa tarde! " + hora2);
 
+         String hora3 = "18horas";
+         System.out.println("Boa noite! " + hora3);
      }
 
 }
